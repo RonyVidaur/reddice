2195eaeea7f1417f93faaf25a726aad93a09b0de
RonyVidaur <rony.vidaur@gmail.com>
2016-12-17 18:35:44 -0600

    now user flash message received on redux devTools works

--- client/reducers/flashMessages.js
+++ client/reducers/flashMessages.js
@@ -1,13 +1,13 @@
 import { ADD_FLASH_MESSAGE } from '../actions/types'
-import shortId from 'shortid'
+import shortid from 'shortid'
 
-export default (state = [], action ={}) => {
+export default (state = [], action = {}) => {
   switch (action.type) {
     case ADD_FLASH_MESSAGE:
       return [
         ...state,
         {
-          id: shortId.generate(),
+          id: shortid.generate(),
           type: action.message.type,
           text: action.message.text
         }
